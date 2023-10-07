932dcd9a76b638cf9bcb745b97d0dde113d98729
David Lay <davidjlay2277@gmail.com>
2023-10-07 12:55:46 -0500

    prob 2 started

---
+++ uniqueChar.js
@@ -0,0 +1,17 @@
+const perf = require('execution-time')();
+let arrayTest = [];
+
+function createArrayTest2(size){
+    let arrayTest = [];
+    for (let i=0; i<size; i++){
+        arrayTest.push(i);
+    }
+    return arrayTest
+}
+
+const str1 = "The quick brown fox jumps over the lazy dog!
+const str2 = "I like cats, but not mice"
+
+isPangram = () => {
+
+}
